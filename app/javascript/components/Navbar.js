23def20f5f0dd12fe7dc7520c7dbcaae9f498586
Marcle Rodrigues <marcle.rodrigues@codeminer42.com>
2017-06-24 18:58:24 -0300

    Refactor Navbar component to use Stanley template

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -2,29 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="navbar navbar-default">
-    <div className="container-fluid">
+  <div className="navbar navbar-inverse navbar-static-top">
+    <div className="container">
       <div className="navbar-header">
-        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span className="sr-only">Toggle navigation</span>
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
           <span className="icon-bar"></span>
           <span className="icon-bar"></span>
           <span className="icon-bar"></span>
         </button>
-        <Link to="/" className="navbar-brand"> Foozin Blog </Link>
+        <Link to="/" className="navbar-brand">MY BLOG</Link>
       </div>
-
-      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul className="nav navbar-nav">
-          <li className="active">
-            <Link to="/">
-              Home <span className="sr-only">(current)</span>
-            </Link>
-          </li>
+      <div className="navbar-collapse collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li><Link to="/">Home</Link></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="blog.html">Projects</a></li>
+          <li><a href="contact.html">Contact</a></li>
         </ul>
       </div>
     </div>
-  </nav>
+  </div>
 );
 
 export default Navbar;
